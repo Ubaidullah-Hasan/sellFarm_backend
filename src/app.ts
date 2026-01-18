d9f08a47377cc8bd7f68661562d61aea3de428e7
Hasan <hm2964133@gmail.com>
2026-01-18 19:25:26 +0600

    frontend server

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import notFoundRoute from "./app/middlewares/notFoundRoute";
 import router from "./app/routes";
 import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import path from "path";
+
 
 const app: Application = express();
 
@@ -13,10 +15,27 @@ app.use(express.json());
 // routes 
 app.use("/api/v1", router);
 
+// ===============================
+// FRONTEND SERVE
+// ===============================
+
+// frontend folder path
+const frontendPath = path.join(process.cwd(), "frontend");
+
+// static files
+app.use(express.static(frontendPath));
+
+
 // test route
 app.get("/", (req, res) => {
   res.send("Welcome to our server!");
 });
+
+// IMPORTANT: frontend fallback
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
+
 
 app.use(globalErrorHandler);
 
